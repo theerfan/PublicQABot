7e575b7f644704417a4e0ffc168c338848c8aab6
Erfan <Erfanabedi.78@gmail.com>
2019-07-04 21:54:02 +0430

    First public commit. More to come!

---
+++ README.md
@@ -0,0 +1,61 @@
+# PublicQABot
+
+A telegram bot in python which gets questions from users, sends it to a responder and finally sends the question and its answer to a public channel. 
+
+## Getting Started
+
+First things first, clone the project to any directory on your local machine that you want.
+```
+git clone https://github.com/theerfan/PublicQABot.git
+```
+
+### Prerequisites
+
+In order to make use of the project, you'll need to install [Python 3](https://www.python.org/downloads/).
+After adding pip and python to your PATH (for windows users), execute the following command in the project's local folder to install the necessary packages.
+
+```
+pip install -r requirements.txt
+```
+
+## Starting the bot
+
+After putting your token, username of the responder, username of the developer (the one who recieves errors as messages if something goes wrong), and the id of the channel in the code (which should be done by hand),
+run the program like this (put python3 instead of python if you have python 2 and 3 simultaneously), the reason for running it in interactive mode is that you'll probably want to kill it gracefully when you're done with it.
+
+```
+python -i bot.py
+```
+
+## Stopping the bot
+
+Because you've ran in interactive mode, in order to kill it with grace, you'll need to first enter the follwing command:
+```
+updater.stop()
+```
+And then proceed to kill the process.
+
+
+## Built With
+
+* [Python 3](https://www.python.org) 
+* [python-telegram-bot](https://github.com/python-telegram-bot/python-telegram-bot) - Everything that's handled by the bot.
+* [More Features to come!]
+
+
+## Authors
+
+* **Erfan Abedi** - *all the work :)))* - [TheErfan](https://github.com/TheErfan)
+* **Mahdi Sadeghzadeh Ghamsary** - *the starting idea* - [MSadeghzadehG](https://github.com/MSadeghzadehG)
+* **MohammadMahdi Khancherly** - *Debugging, future features.* - [MMKH](https://github.com/mmkhmmkh)
+
+## License
+
+This project is masalan licensed under the MIT License
+
+## Acknowledgments
+
+* Hat tip to anyone whose code was used
+* Inspiration
+* etc
+
---
+++ bot.py
@@ -0,0 +1,159 @@
+﻿import telegram
+from telegram.ext import Updater, Dispatcher, MessageHandler, MessageQueue, CommandHandler, Filters
+
+TOKEN = "REDACTED"
+bot = telegram.Bot(TOKEN)
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
+jq = updater.job_queue
+list_of_active_users = dict()
+FAIL_TEXT = "متاسفانه مشکلی پیش اومد، لطفا دوباره پیام‌تون رو ارسال کنید."
+WELCOME_TEXT = "سلام. به بات پرسش و پاسخ خوش آمدید."
+WAIT_TEXT = "پیام شما دریافت شد؛ لطفا شکیبا باشید تا مسئولین جواب بدند."
+RESPONDED_TEXT = "جوابتون رو اینجا دادیم"
+# must not start with an @
+RESPONDER_ID = "REDACTED"
+# must start with an @
+CHANNEL_ID = "REDACTED"
+DEV_ID = "REDACTED"
+ASKED_TEXT = "پرسیده اند که"
+ANSWERED_TEXT = "و جواب این است که"
+HIDDEN_STATE = "مخفی"
+SHOWING_STATE = "نمایان"
+YOUR_STATE = "وضعیت فعلی شما: "
+
+def start(bot, update):
+    message = update.message
+    chat_id = message.chat_id
+    user = message.from_user.username
+    add_to_users(user, chat_id)
+    bot.send_message(chat_id=chat_id, text=WELCOME_TEXT)
+
+def receive(bot, update):
+    user = username = update.message.from_user
+    username = user.username
+    message = update.message
+    add_to_users(username, message.chat_id)
+    if username == RESPONDER_ID:
+        receive_from_ta(bot, update)
+    else:
+        receive_from_users(bot, update, user, message)
+
+def add_to_users(user, chat_id):
+    if user not in list_of_active_users.keys():
+        list_of_active_users.update({user: {'id': chat_id, 'visible': True}})
+
+def format_tas_outgoing_string(text, answer_text, caption=None):
+    half = txt = ""
+    checkText = text
+    if not text:
+        text = caption
+    splitMessage = text.split('\n')
+    asker = {"name": splitMessage[0], "handle": splitMessage[1][1:]}
+    for i in range(2, len(splitMessage)):
+        half += str(splitMessage[i])
+    if checkText and caption:
+        half += "\n" + caption
+    if list_of_active_users[asker["handle"]]["visible"]:
+        txt = asker["name"]
+    txt += "  " + ASKED_TEXT + ":\n" + half + "\n\n" + ANSWERED_TEXT + ":\n" + answer_text
+    return txt, asker
+
+
+def receive_from_ta(bot, update):
+    sent_in_channel = None
+    answer = update.message
+    message = answer.reply_to_message
+    caption = message.caption
+    answer_text = answer.text
+    txt, asker = format_tas_outgoing_string(message.text, answer_text, caption)
+    sent_in_channel = returnSentMedia(bot, CHANNEL_ID, message, txt)
+    sent_link = sent_in_channel.link
+    bot.send_message(chat_id=list_of_active_users[asker["handle"]]["id"],
+                     text='<a href="' + sent_link + '">' + RESPONDED_TEXT + '</a>',
+                     parse_mode=telegram.ParseMode.HTML)
+
+
+def not_sent_error(bot, message, ex):
+    bot.send_message(chat_id=message.chat_id, text=FAIL_TEXT)
+    bot.send_message(chat_id=list_of_active_users[DEV_ID]["id"], text=str(ex) +"\n" + message.chat_id)
+
+
+def receive_from_users(bot, update, user, message):
+    try:
+        if is_a_registered_member(user):
+            txt = user.full_name + "\n" + "@" + user.username + "\n" + message.text
+            bot.send_message(chat_id=list_of_active_users[RESPONDER_ID]["id"], text=txt)
+            bot.send_message(chat_id=message.chat_id, text=WAIT_TEXT)
+    except Exception as ex:
+        not_sent_error(bot, message, ex)
+
+
+def returnSentMedia(bot, ta_id, message, txt):
+    sent_in_channel = None
+    if message.video:
+        sent_in_channel = bot.send_video(chat_id=ta_id , video=message.video.file_id, caption=txt)
+    if message.photo:
+        photos = message.photo
+        lastPhoto = len(photos) - 1
+        sent_in_channel = bot.send_photo(chat_id=ta_id, photo=message.photo[lastPhoto].file_id, caption=txt)
+    if message.audio:
+        sent_in_channel = bot.send_audio(chat_id=ta_id, photo=message.audio.file_id, caption=txt)
+    if message.document:
+        sent_in_channel = bot.send_document(chat_id=ta_id, photo=message.document.file_id, caption=txt)
+    if not sent_in_channel:
+        sent_in_channel = bot.send_message(chat_id=ta_id, text=txt)
+    return sent_in_channel
+
+
+def forward_media(bot, update):
+    ta_id = chat_id = list_of_active_users[RESPONDER_ID]["id"]
+    message = update.message
+    user = message.from_user
+    username = user.username
+    chat_id = message.chat_id
+    add_to_users(username, chat_id)
+    txt = ""
+    if username != RESPONDER_ID:
+        try:
+            if message.caption:
+                txt += message.caption
+            if message.text and txt != "":
+                txt += "\n" + message.text
+            txt = user.full_name + "\n" + "@" + username + "\n" + txt
+            returnSentMedia(bot, ta_id, message, txt)
+            bot.send_message(chat_id=message.chat_id, text=WAIT_TEXT)
+        except Exception as ex:
+            not_sent_error(bot, message, ex)
+
+
+def is_a_registered_member(user):
+    '''
+    Will be changed if in a future event we only want the opinion of the participants,
+    As of April 3rd, 2019 it's being used for an "Asrane" event, so there's no use to it.
+    '''
+    return True
+
+def toggle_name_visibility(bot, update):
+    message = update.message
+    user = message.from_user
+    username = user.username
+    chat_id = message.chat_id
+   
+    if username in list_of_active_users.keys():
+        user_in_list = list_of_active_users[username]
+        user_in_list["visible"] = not user_in_list["visible"] 
+        if user_in_list["visible"]:
+            bot.send_message(chat_id=chat_id, text=YOUR_STATE + SHOWING_STATE)
+        else:
+            bot.send_message(chat_id=chat_id, text=YOUR_STATE + HIDDEN_STATE)
+        
+
+
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('toggle', toggle_name_visibility))
+dispatcher.add_handler(MessageHandler(Filters.text, receive))
+dispatcher.add_handler(MessageHandler(Filters.audio | Filters.video | Filters.photo | Filters.document, forward_media))
+
+
+updater.start_polling()
